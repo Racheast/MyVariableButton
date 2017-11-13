e3730055b3b5792e7e85e99d1470131de47f3013
Alexander Eigner <alexander.eigner@gmm.local>
2017-11-13 11:38:23 +0100

    Removed unneccesary code. Updated description qext file.

--- AdvancedRangeSlider/AdvancedRangeSlider.js
+++ AdvancedRangeSlider/AdvancedRangeSlider.js
@@ -24,15 +24,9 @@ define( [
 			if(layout.props.intOrDec == false){
 				nrOfSteps = 1;
 			}
+			
 			qlik.currApp().variable.setStringValue(layout.props.variableLowerBound, layout.props.selectedValueLowerBound + "");
 			qlik.currApp().variable.setStringValue(layout.props.variableUpperBound, layout.props.selectedValueUpperBound + "");
-			/*
-			html += '<div id="qs-slider-ext-' + id + '">';
-			html += 	'<div id ="labels"><span id="min-val">' + layout.props.minValue + '</span><span id="max-val">' + layout.props.maxValue + '</span></div>';
-			html += 	'<input id="qs-slider-' + id + '" type="range" min="' + layout.props.minValue + '" max="' + layout.props.maxValue + '" step="' + nrOfSteps + '" style="width:95%" value="' + layout.props.selectedValue + '" list="tickmarks"/>';
-			html += '</div>';			
-			html += printDatalist(layout.props.minValue, layout.props.maxValue, layout.props.nrOfPartitions);
-			*/
 			
 			$( function() {
 				$( "#slider-range" ).slider({
@@ -44,7 +38,6 @@ define( [
 						$( "#amount" ).val( "" + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
 					  },
 					  change: function( event, ui ) {
-						//$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
 						layout.props.selectedValueLowerBound = $( "#slider-range" ).slider( "values", 0 ) + "";
 						qlik.currApp().variable.setStringValue(layout.props.variableLowerBound, layout.props.selectedValueLowerBound);
 						 
@@ -66,28 +59,9 @@ define( [
 			
 			$element.html(html);
 			
-			
-			
 			//needed for export
 			return qlik.Promise.resolve();
 		}
 	};
 
 } );
-
-
-function printDatalist(minValue, maxValue, nrOfPartitions){
-	var datalist="<datalist id=tickmarks>";
-	var range = maxValue - minValue;
-	var intervall = parseFloat(range / nrOfPartitions);
-	var tickValue = minValue;
-	if(nrOfPartitions > 0){
-		for(var i=0; i<(nrOfPartitions-1); i++){
-				tickValue = parseFloat(tickValue) + intervall;
-				tickValue = Math.round(tickValue*100) / 100;
-				datalist += '<option>' + tickValue + '</option>';
-		}
-	}
-	datalist += "</datalist>";
-	return datalist;
-}
