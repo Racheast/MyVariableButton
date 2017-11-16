94f92e5b8c9fd7988522164e6d81aa83aa5cb980
Alexander Eigner <alexander.eigner@gmm.local>
2017-11-16 08:41:27 +0100

    Changed type of properties minValue,maxValue,selectedLowerValue,selectedUpperValue to fx=optional. Value of all fx properties (also standard properties) do not show up in the property panel in QlikSense Desktop. Removed unnecessary code from properties.js.

--- AdvancedRangeSlider/properties.js
+++ AdvancedRangeSlider/properties.js
@@ -68,54 +68,31 @@ define(["qlik"], function(qlik) {
                             minValue: {
                                 ref: "props.minValue",
                                 label: "Minimum Value",
-                                type: "string",
-                                defaultValue: "0"
+                                type: "number",
+                                defaultValue: "0",
+								expression: "optional"
                             },
                             maxValue: {
                                 ref: "props.maxValue",
                                 label: "Maximum Value",
-                                type: "string",
-                                defaultValue: "100"
+                                type: "number",
+                                defaultValue: "100",
+								expression: "optional"
                             },
-                            /*
-							nrOfPartitions: {
-                                ref: "props.nrOfPartitions",
-                                label: "Nr. of partitions",
-                                type: "string",
-                                defaultValue: "5"
-                            },
-                            
-							partitionWarning: {
-                                label: "Warning! Partitions may be displayed wrongly if integer-mode is on.",
-                                component: "text"
-                            },
-                            
-							intOrDec: {
-                                type: "boolean",
-                                component: "switch",
-                                label: "integer / decimal",
-                                ref: "props.intOrDec",
-                                options: [{
-                                    value: true,
-                                    label: "decimal"
-                                }, {
-                                    value: false,
-                                    label: "integer"
-                                }],
-                                defaultValue: true
-                            }
-							,*/
                             selectedValueLowerBound: {
                                 ref: "props.selectedValueLowerBound",
                                 label: "Lower Selected Value",
-                                type: "string",
-                                defaultValue: "10"
+                                type: "number",
+                                defaultValue: "10",
+								expression: "optional",
+								show: "true"
                             },
 							selectedValueUpperBound: {
                                 ref: "props.selectedValueUpperBound",
                                 label: "Upper Selected Value",
-                                type: "string",
-                                defaultValue: "90"
+                                type: "number",
+                                defaultValue: "90",
+								expression: "optional"
                             }
 							
                         }
