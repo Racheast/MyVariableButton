b6213a2071b93f61d4a990ae80cfa99d2a8ddb62
Alexander Eigner <alexander.eigner@gmm.local>
2017-12-05 15:32:40 +0100

    Added segment-name textfield. Commented out unneccesary buttons and functions.

--- MailChimpButton/MailChimpButton.js
+++ MailChimpButton/MailChimpButton.js
@@ -1,10 +1,9 @@
 define( [
 		"qlik",
 		"./properties",
-		'text!./style.css',
 		"jquery"
 	],
-	function ( qlik, props, cssContent, $) {
+	function ( qlik, props, $) {
 
 	return {
 		initialProperties : {  
@@ -26,17 +25,12 @@ define( [
 			exportData : false
 		},
 		paint: function ($element, layout) {
-			var html = '<div><button type="submit" id="addUpdateUsers" >Add/update users to a list</button></div>';
-			html += '<div><button type="submit" id="addToSegment" >Add email-addresses to a segment</button></div>';
-			html += '<div><button type="submit" id="getSomething" >get something</button></div>';
 			
-			var segment = {
-				"name" : "myJSsegment",
-				"static_segment" : []
-			};				
-			
-			var members_to_add = ["mmmuster@mustermail.ed","ssspiderman@spinne.ed"];
-			var members_to_remove = [];
+			var html = '<div><input type="text" id="segmentName" placeholder="Segmentname" style="width:40%"></div>';
+			html += '<div><button type="submit" id="addToSegment" style="width:40%">Add email-addresses to a segment</button></div>';
+			//html += '<div><button type="submit" id="addUpdateUsers" >Add/update users to a list</button></div>';
+			//html += '<div><button type="submit" id="getSomething" >get something</button></div>';
+							
 			
 			//USEFUL !!!
 			//https://community.qlik.com/blogs/qlikviewdesignblog/2016/10/14/getting-all-data-cells-from-appcreatecube
@@ -45,47 +39,56 @@ define( [
 			
 			$("#addToSegment").click(function(){
 				console.log("trying post...");
+				var segmentName = $("#segmentName").val();
 				
-				//NICE-TO-HAVE: AppObject-picker for dynamically selecting the table (like in SetObjectState)			
-				qlik.currApp().getObject('dzJ').then(function(model){
-					var totalheight = model.layout.qHyperCube.qSize.qcy;  
-					var pageheight = 500;
-					var nrOfPages = Math.ceil(totalheight/pageheight);					
-					var tasks = [];
-					var emailaddresses = [];
+				if(segmentName.length != 0){	
+					var segment = {
+						"name" : segmentName,
+						"static_segment" : []
+					};
 					
-					for(var i=0; i < nrOfPages; i++){
-						console.log("Creating page ...");
-						var page = {
-							qTop: i * pageheight, 
-							qWidth: 20, 
-							qLeft: 0, 
-							qHeight: pageheight
-						};
-						tasks.push(model.getHyperCubePivotData('/qHyperCubeDef', [page]));
-					}
-					
-					Promise.all(tasks).then((pages) => {
+					//NICE-TO-HAVE: AppObject-picker for dynamically selecting the table (like in SetObjectState)			
+					qlik.currApp().getObject('dzJ').then(function(model){
+						var totalheight = model.layout.qHyperCube.qSize.qcy;  
+						var pageheight = 500;
+						var nrOfPages = Math.ceil(totalheight/pageheight);					
+						var tasks = [];
+						var emailaddresses = [];
 						
-						for(var i=0; i < pages.length; i++){
-							var page = pages[i];
-							for(var j=0; j < page[0].qLeft.length; j++){
-								var email_address = page[0].qLeft[j].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qText;
-								if(email_address != "-"){
-									emailaddresses.push(email_address);
-								}
-							}
+						for(var i=0; i < nrOfPages; i++){
+							console.log("Creating page ...");
+							var page = {
+								qTop: i * pageheight, 
+								qWidth: 20, 
+								qLeft: 0, 
+								qHeight: pageheight
+							};
+							tasks.push(model.getHyperCubePivotData('/qHyperCubeDef', [page]));
 						}
 						
-						return emailaddresses;
-					}).then((emailaddresses) => {  //send to MailChimpRestProxy
-						
-						createSegment(segment, emailaddresses);
+						Promise.all(tasks).then((pages) => {
+							
+							for(var i=0; i < pages.length; i++){
+								var page = pages[i];
+								for(var j=0; j < page[0].qLeft.length; j++){
+									var email_address = page[0].qLeft[j].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qText;
+									if(email_address != "-"){
+										emailaddresses.push(email_address);
+									}
+								}
+							}
+							
+							return emailaddresses;
+						}).then((emailaddresses) => {  //send to MailChimpRestProxy
+							
+							createSegment(segment, emailaddresses);
+						});
 					});
-				});
-				
+				}else{
+					alert("Please enter a segment name.");
+				}
 			});
-			
+			/* WORKING CODE: UNUSED ATM 
 			$("#addUpdateUsers").click(function(){
 				//NICE-TO-HAVE: AppObject-picker for dynamically selecting the table (like in SetObjectState)			
 				qlik.currApp().getObject('dzJ').then(function(model){
@@ -117,9 +120,8 @@ define( [
 								var zipcode = page[0].qLeft[j].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qText;
 								var address = page[0].qLeft[j].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qText;
 								var email_address = page[0].qLeft[j].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qSubNodes[0].qText;
-								/*
-								 * !!! REPLACE THIS WITH DYNAMIC CODE !!!
-								 */
+								
+								 // !!! REPLACE THIS WITH DYNAMIC CODE !!!
 								var status_if_new = "subscribed";
 								if(email_address != "-"){
 									var member = {
@@ -155,19 +157,9 @@ define( [
 					}
 				});
 			});
-			/*
-			MemberDto:
-			{"email_address":"b4kbn58k5bblr4@jmf7z8fgonq.com","merge_fields":{"FNAME":"Vorname","LNAME":"Otto"},"status_if_new":"subscribed"}
-
-			MemberDto:
-			{"email_address":"ki7t1s0k2usio@l1quw6mml6oyb.de","merge_fields":{"FNAME":"David","LNAME":"Vogel"},"status_if_new":"subscribed"}
-
-			MemberDto:
-			{"email_address":"k66f7c3np3jlz0glp@lcqpywvty16n3z0.de","merge_fields":{"FNAME":"Jürgen","LNAME":"Müller"},"status_if_new":"subscribed"}
 			
-			My first Batch with these 3 users:
-			"id":"6adb12eace","response_body_url":"","status":"pending"
-			*/
+			
+			
 			function addOrUpdateListMembers(members){
 				console.log("Calling addMembersToList...");
 				$.ajax({
@@ -184,7 +176,7 @@ define( [
 					}
 				});
 			}
-			
+			*/
 			function createSegment(segment, emailaddresses){
 				console.log("Calling createSegment...");
 				$.ajax({
@@ -224,29 +216,6 @@ define( [
 					}
 				});
 			}
-			
-			//create segment
-			//POST https://us13.api.mailchimp.com/3.0/lists/8af007a903/segments
-			//addOrRemoveMembers to created segment
-			//POST https://us13.api.mailchimp.com/3.0/lists/8af007a903/segments/<segmentID>
-			
-			/*
-			$(document).ready(function(){
-
-				$.post('localhost:5000/registrar', {
-				  "enrollId": "jim",
-				  "enrollSecret": "6avZQLwcUe9b"
-				}, function(serverResponse){
-
-				//do what you want with server response
-
-				})
-
-			})
-			*/
-			
-			//needed for export
-			//return qlik.Promise.resolve();
 		}
 	};
 
