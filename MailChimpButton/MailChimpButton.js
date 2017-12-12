b1a2d7fc64ee3ea130031d21a15ae94b32330b84
Alexander Eigner <alexander.eigner@gmm.local>
2017-12-12 11:12:36 +0100

    Small updates

--- MailChimpButton/MailChimpButton.js
+++ MailChimpButton/MailChimpButton.js
@@ -27,7 +27,8 @@ define( [
 		paint: function ($element, layout) {
 			
 			var html = '<div><input type="text" id="segmentName" placeholder="Segmentname" style="width:40%"></div>';
-			html += '<div><button type="submit" id="addToSegment" style="width:40%">Add email-addresses to a segment</button></div>';
+			html += '<div><button type="submit" id="addToSegment" style="width:40%">' + getMessage(layout.props.languageChoice, "buttonText") + '</button></div>';
+			//html += '<div id="dialog" title="Basic dialog"><p>This is the default dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the "x" icon.</p></div>';
 			//html += '<div><button type="submit" id="addUpdateUsers" >Add/update users to a list</button></div>';
 			//html += '<div><button type="submit" id="getSomething" >get something</button></div>';
 							
@@ -36,6 +37,36 @@ define( [
 			//https://community.qlik.com/blogs/qlikviewdesignblog/2016/10/14/getting-all-data-cells-from-appcreatecube
 			
 			$element.html(html);  //works only in paint()
+			
+			function getMessage(languageChoice, type){
+				var message = "";
+				if(languageChoice == "DE"){
+					switch(type){
+						case "success":
+							message = "Segment wurde erstellt.";
+							break;
+						case "noSegmentName":
+							message = "Bitte geben Sie einen Segmentnamen an.";
+							break;
+						case "buttonText":
+							message = "Segment mit Email-Adressen erstellen"
+							break;
+					}
+				}else if(languageChoice == "EN"){
+					switch(type){
+						case "success":
+							message = "Segment has been created.";
+							break;
+						case "noSegmentName":
+							message = "Please enter a segment name.";
+							break;
+						case "buttonText":
+							message = "create segment with email-addresses";
+							break;
+					}
+				}
+				return message;
+			}
 			
 			$("#addToSegment").click(function(){
 				console.log("trying post...");
@@ -177,6 +208,7 @@ define( [
 				});
 			}
 			*/
+			
 			function createSegment(segment, emailaddresses){
 				console.log("Calling createSegment...");
 				$.ajax({
@@ -191,16 +223,20 @@ define( [
 					dataType: 'json',
 					success: function(data) {
 						console.log("createSegment: SUCCESS: data: " + JSON.stringify(data));
-						alert("Segment with the name " + data.name + " has been created.");
+						alert(getMessage(layout.props.languageChoice,"success"));
 						var segmentID = data.id;
 						var membersToAddAndRemove = {
 							"members_to_add" : emailaddresses,
 							"members_to_remove" : []
 						}
 						addOrRemoveMembersFromSegment(membersToAddAndRemove, segmentID);
+					},
+					error: function(data) {
+						console.log("Data.statustext: " + data.statusText);
+						alert(data.statusText);
 					}
 				});
-			}
+			};
 			
 			// !!! MAKE SURE THAT THE EMAIL-ADDRESSES BELONG TO USERS THAT EXIST IN MAILCHIMP !!!
 			function addOrRemoveMembersFromSegment(membersToAddAndRemove, segmentID){
--- MailChimpButton/properties.js
+++ MailChimpButton/properties.js
@@ -7,19 +7,36 @@ define(["qlik"], function(qlik) {
             settings: {
                 uses: "settings",
                 items: {
-                    sliderSettingsHeader: {
-                        type: "items",
-                        label: "Slider Settings",
-                        items: {
-                            
-                            selectedValue: {
-                                ref: "props.selectedValue",
-                                label: "Selected Value",
-                                type: "string",
-                                defaultValue: "50"
-                            }
-                        }
-                    }
+					languages: {  //dropdown maybe causes very long loading times of variables if there's plenty of them
+						   label: "Language choice",
+						   type: "string",
+						   component: "dropdown",
+						   options: function() {
+							  return[{value: "DE", label: "DE"},{value: "EN", label: "EN"}];
+						   },
+						   expression: "always",
+						   ref: "props.languageChoice"
+						},
+					mailChimpRestSettings: {
+							label:"MailChimp Rest Settings",
+							items: {
+								baseURL: {
+									label: "Base-URL",
+									type: "string",
+									ref: "props.baseURL"
+								},
+								apiPrefix: {
+									label: "API Prefix",
+									type: "string",
+									ref: "props.apiPrefix"
+								},
+								mailchimpKey: {
+									label: "MailChimp Key",
+									type: "string",
+									ref: "props.mailchimpKey"
+								}
+							}
+					}
                 }
             }
         }
