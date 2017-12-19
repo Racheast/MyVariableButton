e3e4f8b76f5f026b73c842e51b2768490760a41e
Alexander Eigner <alexander.eigner@gmm.local>
2017-12-19 10:08:55 +0100

    First version of the advanced slider based on noUiSlider. Tooltip works, no qlik-sense css styling issues so far.

---
+++ AdvancedSlider_noUI/AdvancedSlider_noUI.js
@@ -0,0 +1,56 @@
+define( [
+		"qlik",
+		"./properties",
+		"./initialproperties",
+		"jquery",
+		'text!./css/nouislider.css',
+		'./js/nouislider',
+		'./js/wnumb-1.1.0/wNumb'
+	],
+	function ( qlik, props, initprops, $,noUISliderCSS,noUiSlider,wNumb) {
+		$("<style>").html(noUISliderCSS).appendTo("head");
+	return {
+		definition:props,
+		initialproperties: initprops,
+		support : {
+			snapshot: true,
+			export: true,
+			exportData : false
+		},
+		paint: function ($element, layout) {
+			//set the variable to the last known selected value
+			qlik.currApp().variable.setStringValue(layout.props.variable, layout.props.selectedValue);
+			var html='<br/><br/><br/><div id="slider" style="float:none;"></div>';
+			$element.html(html);
+			
+			var slider = document.getElementById('slider');
+			
+			noUiSlider.create(slider, {
+				start: [layout.props.selectedValue],
+				connect: true,
+				range: {
+					'min': parseInt(layout.props.minValue),
+					'max': parseInt(layout.props.maxValue)
+				},
+				step: parseInt(layout.props.steps),
+				tooltips: wNumb({ decimals: 0 }),
+				pips: { // Show a scale with the slider
+					mode: 'count',
+					stepped: true,
+					density: parseInt(layout.props.pipsDensity),
+					values: parseInt(layout.props.nrOfPipsValues)
+				}
+			});
+			
+			slider.noUiSlider.on('change', function(values, handle){
+				layout.props.selectedValue =values[handle];
+				qlik.currApp().variable.setStringValue(layout.props.variable, layout.props.selectedValue);
+			});
+			
+			//needed for export
+			return qlik.Promise.resolve();
+		}
+	};
+
+} );
+
---
+++ AdvancedSlider_noUI/properties.js
@@ -0,0 +1,82 @@
+define(["qlik"], function(qlik) {
+
+    return {
+        type: "items",
+        component: "accordion",
+        items: {
+            settings: {
+                uses: "settings",
+                items: {
+                    sliderSettingsHeader: {
+                        type: "items",
+                        label: "Slider Settings",
+                        items: {
+                            variable: {
+                                label: "Variable",
+                                type: "string",
+                                component: "dropdown",
+                                options: function() {
+                                    var app = qlik.currApp();
+                                    var varList = [];
+
+                                    return app.getList("VariableList").then(function(items) {
+                                        items.layout.qVariableList.qItems.forEach(function(item) {
+                                            varList.push({
+                                                value: item.qName,
+                                                label: item.qName
+                                            });
+                                        });
+										varList.sort(function(a, b){
+											if(a.value < b.value) return -1;
+											if(a.value > b.value) return 1;
+											return 0;
+										})
+                                        return varList;
+                                    });
+                                },
+                                expression: "always",
+                                ref: "props.variable"
+                            },
+                            minValue: {
+                                ref: "props.minValue",
+                                label: "Minimum Value",
+                                type: "integer",
+                                defaultValue: "0"
+                            },
+                            maxValue: {
+                                ref: "props.maxValue",
+                                label: "Maximum Value",
+                                type: "integer",
+                                defaultValue: "100"
+                            },
+                            pipsDensity: {
+                                ref: "props.pipsDensity",
+                                label: "density of the slider marks",
+                                type: "integer",
+                                defaultValue: "1"
+                            },
+							nrOfPipsValues: {
+								ref: "props.nrOfPipsValues",
+                                label: "Nr of labeled slider marks",
+                                type: "integer",
+                                defaultValue: "5"
+							},
+                            steps: {
+                                ref: "props.steps",
+                                label: "Slider steps",
+                                type: "integer",
+                                defaultValue: "1"
+                            },
+                            selectedValue: {
+                                ref: "props.selectedValue",
+                                label: "Selected Value",
+                                type: "string",
+                                defaultValue: "50"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
