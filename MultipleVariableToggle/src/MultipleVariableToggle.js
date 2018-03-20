070bf5b2c7bde6b1e9ea29be95ce4feb733a0243
Alexander Eigner <alexander.eigner@gmm.local>
2018-03-20 11:17:38 +0100

    Added and tested the State Variable functionality. Therefor implemented getVariableContent. State Variable is a global variable that stores a MultipleVariableToggle's selected toggle index.

--- MultipleVariableToggle/src/MultipleVariableToggle.js
+++ MultipleVariableToggle/src/MultipleVariableToggle.js
@@ -15,21 +15,63 @@ define(["text!./MultipleVariableToggle.ng.html",
 
          var app = qlik.currApp();
 		 
-		 if($scope.layout.toggledIndex > -1){  // A toggle has been selected
-			var array = $scope.layout.variableToggle[$scope.layout.toggledIndex].variableArray;
-			for(var i=0; i<array.length;i++){
-				app.variable.setStringValue(array[i].variableName,array[i].value);
-			}
+		 if($scope.layout.stateVariable != undefined && $scope.layout.stateVariable.length > 0){  //stateVariable has been set
+			 getVariableContent($scope.layout.stateVariable).then( (lastToggledIndex)=>{
+				 if(lastToggledIndex != undefined && lastToggledIndex != "-"){
+					$scope.layout.toggledIndex = lastToggledIndex;
+					setVariablesForToggle($scope.layout.toggledIndex);
+				 }
+				
+			 });
+		 }else{  //stateVariable hasn't been set
+			 if($scope.layout.toggledIndex > -1){  // A toggle has been selected
+				setVariablesForToggle($scope.layout.toggledIndex);
+			 }
 		 }
 		 
          $scope.toggleVar = function($index) {  //Function started when a toggle with an $index is clicked in the html template
-            $scope.layout.toggledIndex = $index;
-			var array = $scope.layout.variableToggle[$scope.layout.toggledIndex].variableArray;
-			for(var i=0; i<array.length;i++){
-				app.variable.setStringValue(array[i].variableName,array[i].value);
-			}
+			app.variable.setStringValue($scope.layout.stateVariable,"" + $index);  //setting the global stateVariable that saves the state of all MultipleVariableToggles that are assigned to it
+			$scope.layout.toggledIndex = $index;  
+			setVariablesForToggle($scope.layout.toggledIndex);
          }
 		 
+		 function setVariablesForToggle(index){
+			if(index >= 0){	
+				var array = $scope.layout.variableToggle[index].variableArray;
+				for(var i=0; i<array.length;i++){
+					app.variable.setStringValue(array[i].variableName,array[i].value);
+				}
+			}else{
+				console.log("setVariablesForToggle: toggledIndex < 0 !");
+			}
+		 }
+		 
+		 function getVariableContent(variableName) {				
+				return new Promise(resolve => {
+					qlik.currApp().createCube({
+						"qDimensions": [{
+							"qDef": {
+								"qFieldDefs": ["Dummy"]
+							}
+						}],
+						"qMeasures": [{
+							"qDef": {
+								"qDef": "=$(" + variableName + ")",
+								"qLabel": variableName
+							}
+						}],
+						"qInitialDataFetch": [{
+							qHeight: 1,
+							qWidth: 2
+						}]
+					}, function(reply) {
+						$.each(reply.qHyperCube.qDataPages[0].qMatrix, function(index, value) {						
+							var valueArrayText = [];
+							resolve(this[1].qText);
+						});
+					});
+				});
+		 }
 		 
       }
    }
--- MultipleVariableToggle/src/properties.js
+++ MultipleVariableToggle/src/properties.js
@@ -79,7 +79,37 @@ define(["qlik"], function(qlik) {
 					ref: "toggledIndex",
 					label: "toggledIndex",
 					defaultValue: "-1"
+				},
+				info:{
+					label:"IMPORTANT: A State Variable is a variable that stores the index of this MultipleVariableToggle's selected toggle-button. Toggle-buttons are indexed according to their listing order. All MultipleVariableToggles that refer to the same State Variable will automatically have selected a toggle-button that has the same index as the one stored in State Variable. Make sure that only IDENTICAL MultipleVariableToggles refer to the same State Variable!",
+					component: "text"
+				},
+				stateVariable: {  //dropdown maybe causes very long loading times of variables if there's plenty of them
+						   label: "State Variable (optional)",
+						   type: "string",
+						   component: "dropdown",
+						   options: function() {
+							  var app = qlik.currApp();
+							  var varList = [];
+
+							  return app.getList("VariableList").then(function(items) {
+								 items.layout.qVariableList.qItems.forEach(function(item) {
+									varList.push({
+									   value: item.qName,
+									   label: item.qName
+									});
+								 });
+								 return varList;
+							  });
+						   },
+						   expression: "always",
+						   ref: "stateVariable"
 				}
+				/*stateVariable:{
+					type: "string",
+					ref: "stateVariable",
+					label: "State Variable"
+				}*/
              }
           },
           settings: {
